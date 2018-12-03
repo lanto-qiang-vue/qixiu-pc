2362872803e1ea0924e3c0523b337b4b4d9aafbd
lanto <lanto@lantodeMacBook.local>
2018-12-03 09:38:49 +0800

    12.3

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const prodConfig={
 //开发测试环境配置
 const devConfig={
   port: '3000',
-  apiUrl: 'http://192.168.169.190:8888/',
-  // apiUrl: 'http://118.25.81.63:8888/',
+  // apiUrl: 'http://192.168.169.190:8888/',
+  apiUrl: 'http://118.25.81.63:8888/',
   // apiUrl: 'http://212.64.5.54:8888/',
 
   repairUrl: 'http://115.159.101.204:7210/',
