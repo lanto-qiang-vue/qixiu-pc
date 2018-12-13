04865e9fab5503aa2ea6a4325019527d2614626e
lanto <lanto@lantodeMacBook.local>
2018-12-13 10:40:45 +0800

    12.13

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const prodConfig={
   // apiUrl: 'http://212.64.5.54:8888/',
   apiUrl: 'http://gateway.qixiu.lanto.com/',
   //查修接口地址
-  repairUrl: 'http://115.159.101.204:7210/',
+  repairUrl: 'http://118.25.35.172:7210/',
   socketUrl: 'https://socket.shanghaiqixiu.org/micro/heatmap/socket',
 }
 
@@ -14,11 +14,11 @@ const prodConfig={
 //开发测试环境配置
 const devConfig={
   port: '3000',
-  apiUrl: 'http://192.168.169.236:8888/',
+  apiUrl: 'http://192.168.169.233:8888/',
   // apiUrl: 'http://118.25.81.63:8888/',
   // apiUrl: 'http://212.64.5.54:8888/',
 
-  repairUrl: 'http://115.159.101.204:7210/',
+  repairUrl: 'http://118.25.35.172:7210/',
   socketUrl: 'http://118.25.131.29:10001/micro/heatmap/socket',
 }
 
