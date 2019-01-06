9b7bd52a2cc83411bd84e49d13170d7ea2727843
lanto <lanto@lantodeMacBook.local>
2019-01-06 19:56:44 +0800

    1.6

--- components/menu/icon-block.vue
+++ components/menu/icon-block.vue
@@ -13,7 +13,7 @@
     <nuxt-link tag="a" to="/service-map?type=215"><img src="/img/menu-icon/新能源汽车维修.png"/><p>新能源汽车维修</p></nuxt-link>
     <nuxt-link tag="a" to="/article/help"><img src="/img/menu-icon/道路救援.png"/><p>施救牵引企业</p></nuxt-link>
     <!--<nuxt-link tag="a" to="/guild-article/10281034" ><img src="/img/menu-icon/推介服务.png"/><p>推介服务</p></nuxt-link>-->
-    <nuxt-link tag="a" to="/service-map?type=300" ><img src="/img/menu-icon/推介服务.png"/><p>学驾推荐</p></nuxt-link>
+    <nuxt-link tag="a" to="/service-map?type=300" ><img src="/img/menu-icon/推介服务.png"/><p>学车找我</p></nuxt-link>
     <a href="/file/tips.pdf" ><img src="/img/menu-icon/行业概况.png"/><p>爱车小贴士</p></a>
   </div>
   <div class="block" v-show="type==2">
