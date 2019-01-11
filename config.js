5f9a8dc31267789f9137a7d8f3aafac5622a10de
lanto <lanto@lantodeMacBook.local>
2019-01-11 13:59:29 +0800

    1.11

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const prodConfig={
   apiUrl: 'http://gateway.qixiu.lanto.com/',
   //查修接口地址
   repairUrl: 'http://118.25.35.172:7210/',
-  socketUrl: 'https://socket.shanghaiqixiu.org/heatmap/socket',
-  shandongSocket: 'https://www.shanghaiqixiu.org/heatmap/socket',
+  socketUrl: ' https://www.shanghaiqixiu.org/heatmap/socket',
+  shandongSocket: 'https://www.shandongqixiu.com/heatmap/socket',
   articlePath: 'https://download.image.shanghaiqixiu.org/',
   // articlePath: 'http://download.image.lanto.com/'
   workOn: 'pPro'
@@ -24,8 +24,8 @@ const devConfig={
 
   // repairUrl: 'http://118.25.35.172:7210/',
   repairUrl: 'http://192.168.169.230:7210/',
-  socketUrl: 'http://118.25.131.29:10001/heatmap/socket',
-  shandongSocket: 'http://118.25.13.140:10001/heatmap/socket',
+  socketUrl: ' https://www.shanghaiqixiu.org/heatmap/socket',
+  shandongSocket: 'https://www.shandongqixiu.com/heatmap/socket',
   articlePath: 'http://download.image.lanto.com/',
   workOn: 'pDev'
 }
