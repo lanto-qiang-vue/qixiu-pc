db0d8aebb8dcc56bc9414fd3b9dd351a0441a105
lanto <lanto@lantodeMacBook.local>
2019-01-14 09:51:14 +0800

    1.14

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const prodConfig={
   // apiUrl: 'http://212.64.5.54:8888/',
   apiUrl: 'http://gateway.qixiu.lanto.com/',
   //查修接口地址
-  repairUrl: 'http://corp.search.shanghaiqixiu.org/',
+  // repairUrl: 'http://corp.search.shanghaiqixiu.org/',
+  repairUrl: 'http://129.211.14.78：7210/',
 
   socketUrl: 'https://www.test.shanghaiqixiu.org/heatmap/socket',
   shandongSocket: 'https://www.shandongqixiu.com/heatmap/socket',
