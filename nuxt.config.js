7463935b804cf6d4f660bde9d8accd2d86c24f40
lanto <lanto@lantodeMacBook.local>
2019-01-23 09:34:30 +0800

    1.23

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'universal',
 
   server: {
-    port: config.port,
+    port: config.port ,
     host: '0.0.0.0',
   },
   /*
@@ -18,13 +18,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'user-scalable=no' },
-      { hid: 'description', name: 'description', content: '机动车维修公共服务平台' }
+      { name: 'keywords', content: '上海市机动车维修公共服务平台,汽车维修,汽车养护,汽车保险,上海汽修平台app,上海汽车维修,上海汽车维修保养,上海汽车保养' },
+      { hid: 'description', name: 'description', content: '上海汽修平台是为上海地区车主打造的一款手机汽车服务客户端,软件提供汽车维修、汽车养护、汽车保险等一站式服务，上海汽修平台app立足提升行业监管效能、提升公共服务水平，提高行业监管数字化、维修数据档案化、服务质量透明化、诚信经营公开化的目标建设本市机动车维修行业网站。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
       { type: 'text/javascript', src: "/libs/jquery-3.3.1.min.js"},
+      { type: 'text/javascript', src: "/other.js"},
     ],
   },
 
@@ -67,6 +69,7 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+    port: config.port,
     prefix: '/proxy/',
     proxy: true,
   },
