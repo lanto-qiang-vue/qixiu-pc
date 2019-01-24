d8d9feddab19aac92cec323c5d53a65c9d7f07c2
xchen <xchen@lantoev.com>
2019-01-24 10:21:37 +0800

    ''

--- center/operate/change-company-data.vue
+++ center/operate/change-company-data.vue
@@ -5,7 +5,7 @@
 </style>
 <template>
     <div>
-        <Row class="expand-row" v-for="item in row" :key="item">
+        <Row class="expand-row" v-for="item in row" :key="item.changeItem">
             <Col span="8">
                 <span class="expand-key">变更项: </span>
                 <span class="expand-value" >{{ item.changeItem }}</span>
