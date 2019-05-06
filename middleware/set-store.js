fbd4e0b6c607b3f1962dad300ab43a8aebf13bf0
lanto <lanto@lantodeMacBook.local>
2019-05-06 17:58:51 +0800

    5.6

--- middleware/set-store.js
+++ middleware/set-store.js
@@ -1,5 +1,16 @@
 export default function ({store}) {
   if (process.client) {
+    let qixiuvar = localStorage.getItem('qixiuvar'), flag=true
+    if(qixiuvar && parseInt(qixiuvar)==1){
+      flag= false
+    }
+    if(flag) {
+      localStorage.setItem('qixiuvar', 1)
+      localStorage.removeItem('ACCESSTOKEN')
+      localStorage.removeItem('ACCESSMENU')
+      localStorage.removeItem('USERINFO')
+    }
+
     // console.log('set-store')
     let token = localStorage.getItem('ACCESSTOKEN')
     if(!store.state.user.token && token){
@@ -7,5 +18,6 @@ export default function ({store}) {
       store.commit('user/setMenu', JSON.parse(localStorage.getItem('ACCESSMENU')))
       store.commit('user/setUser', JSON.parse(localStorage.getItem('USERINFO')))
     }
+
   }
 }
